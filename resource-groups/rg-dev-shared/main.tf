ffb869fead184af3f866ddd2d748db2bd3212e7e
MG <glowa.mateusz@gmail.com>
2026-02-13 10:21:23 +0100

    chore: generate Terraform + pipelines from devops-app

---
+++ resource-groups/rg-dev-shared/main.tf
@@ -0,0 +1,29 @@
+locals {
+  resource_group_name = "rg-dev-shared"
+  location            = var.location
+  tags                = var.tags
+}
+
+module "avm_Microsoft_Network_virtualNetworks_0" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version = "~> 1.0"
+
+  name                = "vnet-development"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  tags                = local.tags
+
+  # Review and set AVM-specific variables per module documentation
+}
+
+module "avm_Microsoft_Network_virtualNetworks_1" {
+  source  = "Azure/avm-res-network-virtualnetwork/azurerm"
+  version = "~> 1.0"
+
+  name                = "vnet-2-development"
+  location            = local.location
+  resource_group_name = local.resource_group_name
+  tags                = local.tags
+
+  # Review and set AVM-specific variables per module documentation
+}
